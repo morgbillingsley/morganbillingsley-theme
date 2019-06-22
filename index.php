0d1d30817c8ef319d15e37ed31c89cdf81683bc4
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-22 11:57:14 -0700

    contact-form iv commit

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php get_header(); ?>
+    <div style="height:150px;"></div>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <form action="<?php echo esc_url(admin_url("admin-post.php")); ?>" method="post">
                     <input type="hidden" name="action" value="contact">
-                    <div class="input-group">
+                    <div class="form-group">
                         <label for="fname">First Name:</label>
                         <input type="text" class="form-control" name="fname">
                     </div>
-                    <div class="input-group">
+                    <div class="form-group">
                         <label for="lname">Last Name:</label>
                         <input type="text" class="form-control" name="lname">
                     </div>
-                    <div class="input-group">
+                    <div class="form-group">
                         <label for="email">Email:</label>
                         <input type="email" class="form-control" name="email">
                     </div>
-                    <div class="input-group">
+                    <div class="form-group">
                         <label for="phone">Phone:</label>
                         <input type="text" class="form-control" name="phone">
                     </div>
-                    <div class="input-group">
+                    <div class="form-group">
                         <label for="message">Message:</label>
                         <textarea name="message" class="form-control" rows="5"></textarea>
                     </div>
