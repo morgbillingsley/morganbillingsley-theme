731968f1d67411ee17fd974875167cdf0c9fd2e5
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-23 11:25:32 -0700

    contact-page i commit

--- index.php
+++ index.php
@@ -2,27 +2,28 @@
     <div style="height:150px;"></div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-8 mx-auto">
                 <form action="<?php echo esc_url(admin_url("admin-post.php")); ?>" method="post">
                     <input type="hidden" name="action" value="contact">
                     <div class="form-group">
-                        <label for="fname">First Name:</label>
+                        <label for="fname">First Name</label>
                         <input type="text" class="form-control" name="fname">
                     </div>
                     <div class="form-group">
-                        <label for="lname">Last Name:</label>
+                        <label for="lname">Last Name</label>
                         <input type="text" class="form-control" name="lname">
                     </div>
                     <div class="form-group">
-                        <label for="email">Email:</label>
+                        <label for="email">Email</label>
                         <input type="email" class="form-control" name="email">
                     </div>
                     <div class="form-group">
-                        <label for="phone">Phone:</label>
+                        <label for="phone">Phone</label>
+                        <div class="input-group-addon">+1</div>
                         <input type="text" class="form-control" name="phone">
                     </div>
                     <div class="form-group">
-                        <label for="message">Message:</label>
+                        <label for="message">Message</label>
                         <textarea name="message" class="form-control" rows="5"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Send</button>
