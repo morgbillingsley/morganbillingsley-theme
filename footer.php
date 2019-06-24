390780bc9dd70154318ce0d16d067bc54aaab0f4
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-24 11:35:31 -0700

    bootstrap iii commit

--- footer.php
+++ footer.php
@@ -1,17 +1,19 @@
             <div class="footer">
                 <div class="container">
-                    <div class="col-md-7">
-                        <a class="navbar-brand" href="#main"><h1>Morgan<strong>Billingsley</strong></h1></a><br><br><br>
-                        <p> Please carefully read all of my terms and conditions + privacy policy before signing up and/or using my services.</p>
-                        <div class="footer-text">
-                            <p> Copyright &copy <?php echo date('Y'); ?> Morgan Billingsley. All Rights Reserved.</p>
+                    <div class="row">
+                        <div class="col-md-7">
+                            <a class="navbar-brand" href="#main"><h1>Morgan<strong>Billingsley</strong></h1></a><br><br><br>
+                            <p> Please carefully read all of my terms and conditions + privacy policy before signing up and/or using my services.</p>
+                            <div class="footer-text">
+                                <p> Copyright &copy <?php echo date('Y'); ?> Morgan Billingsley. All Rights Reserved.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-5">
-                        <h1>Contact Us</h1>
-                        <p> Use the email or phone number below to get in touch. <br class="hidden-xs"> We'll get back to you ASAP </p>
-                        <a href="mailto:morgan@morganbillingsley.com">morgan@morganbillingsley.com</a>
-                        <p> (208) 918-2941</p>
+                        <div class="col-md-5">
+                            <h1>Contact Us</h1>
+                            <p> Use the email or phone number below to get in touch. <br class="hidden-xs"> We'll get back to you ASAP </p>
+                            <a href="mailto:morgan@morganbillingsley.com">morgan@morganbillingsley.com</a>
+                            <p> (208) 918-2941</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- front-page.php
+++ front-page.php
@@ -19,56 +19,58 @@
                 <h1 class="wow fadeInDown" data-wow-delay="0.1s">What do I Offer?</h1>
                 <p class="wow fadeInDown" data-wow-delay="0.2s"> I will provide everything your business needs to get started online<br class="hidden-xs">
                 from highly advanced database management to free professionail email service. </p>
-                <div class="col-md-4 features-left">
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.2s">
-                        <div class="icon"> <i class="ion-link"></i></div>
-                        <div class="feature-single">
-                            <h1>Domain Name</h1>
-                            <p> When you choose to have me build your website, I will provide you with a free .com or .org domain name </p>
+                <div class="row">
+                    <div class="col-md-4 features-left">
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.2s">
+                            <div class="icon"> <i class="ion-link"></i></div>
+                            <div class="feature-single">
+                                <h1>Domain Name</h1>
+                                <p> When you choose to have me build your website, I will provide you with a free .com or .org domain name </p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.3s">
+                            <div class="icon"> <i class="ion-ios-world"></i></div>
+                            <div class="feature-single">
+                                <h1>Web Hosting</h1>
+                                <p> My website hosting platform is highly advanced and allows me to provide affordable pricing to my clients </p>
+                            </div>
+                        </div>
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.4s">
+                            <div class="icon"> <i class="ion-folder"></i></div>
+                            <div class="feature-single">
+                                <h1>Database Integration</h1>
+                                <p> Your database will have infinite storage capacity and connection to your webpage </p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.3s">
-                        <div class="icon"> <i class="ion-ios-world"></i></div>
-                        <div class="feature-single">
-                            <h1>Web Hosting</h1>
-                            <p> My website hosting platform is highly advanced and allows me to provide affordable pricing to my clients </p>
+                    <div class="col-md-4">
+                        <div class="table">
+                            <div class="tablecell">
+                                <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/images/app_signup.png" alt="App" />
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.4s">
-                        <div class="icon"> <i class="ion-folder"></i></div>
-                        <div class="feature-single">
-                            <h1>Database Integration</h1>
-                            <p> Your database will have infinite storage capacity and connection to your webpage </p>
+                    <div class="col-md-4 features-left">
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.6s">
+                            <div class="icon"> <i class="ion-at"></i></div>
+                            <div class="feature-single">
+                                <h1>Professional Email</h1>
+                                <p> I will set up a full email hub for you and your entire company </p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="table">
-                        <div class="tablecell">
-                            <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/images/app_signup.png" alt="App" />
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.7s">
+                            <div class="icon"> <i class="ion-code"></i></div>
+                            <div class="feature-single">
+                                <h1>Web Application</h1>
+                                <p> Using the most advanced coding techniques, I will program your web app from start to finish </p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-4 features-left">
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.6s">
-                        <div class="icon"> <i class="ion-at"></i></div>
-                        <div class="feature-single">
-                            <h1>Professional Email</h1>
-                            <p> I will set up a full email hub for you and your entire company </p>
-                        </div>
-                    </div>
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.7s">
-                        <div class="icon"> <i class="ion-code"></i></div>
-                        <div class="feature-single">
-                            <h1>Web Application</h1>
-                            <p> Using the most advanced coding techniques, I will program your web app from start to finish </p>
-                        </div>
-                    </div>
-                    <div class="col-md-12 wow fadeInDown" data-wow-delay="0.8s">
-                        <div class="icon"> <i class="ion-gear-a"></i></div>
-                        <div class="feature-single">
-                            <h1>Maintenance</h1>
-                            <p> Don't be worried about keeping your website streamlined and up to date. I will provide full website maintenance. </p>
+                        <div class="col-md-12 wow fadeInDown" data-wow-delay="0.8s">
+                            <div class="icon"> <i class="ion-gear-a"></i></div>
+                            <div class="feature-single">
+                                <h1>Maintenance</h1>
+                                <p> Don't be worried about keeping your website streamlined and up to date. I will provide full website maintenance. </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -76,27 +78,31 @@
         </div>
 
         <div class="split-features">
-            <div class="col-md-6 nopadding">
-                <div class="split-image"> <img class="img-responsive wow fadeIn" src="<?php bloginfo('template_url'); ?>/images/app_image.jpeg" alt="Image" /></div>
-            </div>
-            <div class="col-md-6 nopadding">
-                <div class="split-content">
-                    <h1 class="wow fadeInUp">Streamline Design</h1>
-                    <p class="wow fadeInUp"> Using expert design techniques and customer feedback, I create tailor-made digital real-estate for any company in any industry.</p><br>
-                    <p class="wow fadeInUp">Take a look at some of the <a href="#">websites</a> I have created and let me know which one you like best, so when I create your web page, I know the style that you prefer.</p>
+            <div class="row">
+                <div class="col-md-6 nopadding">
+                    <div class="split-content">
+                        <h1 class="wow fadeInUp">Streamline Design</h1>
+                        <p class="wow fadeInUp"> Using expert design techniques and customer feedback, I create tailor-made digital real-estate for any company in any industry.</p><br>
+                        <p class="wow fadeInUp">Take a look at some of the <a href="#">websites</a> I have created and let me know which one you like best, so when I create your web page, I know the style that you prefer.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 nopadding">
+                    <div class="split-image"> <img class="img-responsive wow fadeIn" src="<?php bloginfo('template_url'); ?>/images/app_image.jpeg" alt="Image" /></div>
                 </div>
             </div>
         </div>
 
         <div class="split-features2">
-            <div class="col-md-6 nopadding">
-                <div class="split-content second">
-                    <h1 class="wow fadeInUp">Powerful Back End</h1>
-                    <p class="wow fadeInUp"> In addition to the highly streamlined design of your website, I will create a back-end for your website that can handle the intense browsing and traffic.</p>
+            <div class="row">
+                <div class="col-md-6 nopadding">
+                    <div class="split-image"> <img class="img-responsive wow fadeIn" src="<?php bloginfo('template_url'); ?>/images/app_image2.jpeg" alt="Image" /></div>
                 </div>
-            </div>
-            <div class="col-md-6 nopadding">
-                <div class="split-image"> <img class="img-responsive wow fadeIn" src="<?php bloginfo('template_url'); ?>/images/app_image2.jpeg" alt="Image" /></div>
+                <div class="col-md-6 nopadding">
+                    <div class="split-content second">
+                        <h1 class="wow fadeInUp">Powerful Back End</h1>
+                        <p class="wow fadeInUp"> In addition to the highly streamlined design of your website, I will create a back-end for your website that can handle the intense browsing and traffic.</p>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -114,30 +120,24 @@
                 <div class="cta-inner">
                     <h1 class="wow fadeInUp" data-wow-delay="0s">Have you visited my social media pages yet?</h1>
                 </div>
-                <div class="row social-links">
-                    <div class="col-xs-3">
-                        <div class="icon"> <a href="https://www.facebook.com/morgbillingsley/"><i class="ion-social-facebook"></i></a></div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="icon"> <a href="https://www.instagram.com/morgbillingsley/"><i class="ion-social-instagram"></i></a></div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="icon"> <a href="https://www.linkedin.com/in/morgan-billingsley/"><i class="ion-social-linkedin"></i></a></div>
-                    </div>
-                    <div class="col-xs-3">
-                        <div class="icon"> <a href="https://twitter.com/MorgBillingsley"><i class="ion-social-twitter"></i></a></div>
-                    </div>
+                <div class="row social-links w-100 text-center">
+                    <a href="https://www.facebook.com/morgbillingsley/"><i class="ion-social-facebook"></i></a>
+                    <a href="https://www.instagram.com/morgbillingsley/"><i class="ion-social-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/morgan-billingsley/"><i class="ion-social-linkedin"></i></a>
+                    <a href="https://twitter.com/MorgBillingsley"><i class="ion-social-twitter"></i></a>
                     </div>
                 </div>
             </div>
             <div id="review-section" class="review-section">
                 <div class="container">
-                    <div class="col-md-10 col-md-offset-2">
-                        <div class="reviews owl-carousel owl-theme">
-                            <div class="review-single"><img class="rounded-circle" src="<?php bloginfo('template_url'); ?>/images/me.jpeg" />
-                                <div class="review-text wow fadeInUp" data-wow-delay="0.2s">
-                                    <p>Morgan is the founder and president of Morgan Billingsley Website Design <br> email him at morgan@morganbillingsley.com or call at (208) 918-2941</p>
-                                    <h3>Morgan Billingsley</h3>
+                    <div class="row">
+                        <div class="col-md-10 mx-auto">
+                            <div class="reviews owl-carousel owl-theme">
+                                <div class="review-single"><img class="rounded-circle" src="<?php bloginfo('template_url'); ?>/images/me.jpeg" />
+                                    <div class="review-text wow fadeInUp" data-wow-delay="0.2s">
+                                        <p>Morgan is the founder and president of Morgan Billingsley Website Design <br> email him at morgan@morganbillingsley.com or call at (208) 918-2941</p>
+                                        <h3>Morgan Billingsley</h3>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -259,7 +259,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="pricing-intro">
+                        <div class="pricing-intro text-center">
                             <h1 class="wow fadeInUp" data-wow-delay="0s">For Custom Plans</h1>
                             <p class="wow fadeInUp" data-wow-delay="0.2s"> Please <a href="#">contact me</a></p>
                         </div>
