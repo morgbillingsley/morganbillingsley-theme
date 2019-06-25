414f30c9bc28328c0c3e378956e25ca5e50a53f5
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-25 09:09:03 -0700

    bootstrap xiii commit

--- js/custom.js
+++ js/custom.js
@@ -20,14 +20,6 @@ jQuery(function($) {
        }
 
       });
-
-
-  // Collapse navbar on click
-
-   $(document).on('click.nav','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-    $(this).removeClass('in').addClass('collapse');
-   }
   });
 
 
