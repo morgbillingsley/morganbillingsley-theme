1cc0036fb36702f3328bb1c0eb4c0449efc9d6bc
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-25 09:17:21 -0700

    bootstrap xv commit

--- header.php
+++ header.php
@@ -58,7 +58,7 @@
 
     <div class="wrapper">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Morgan<span>Billingsley</span></a>
+        <a class="navbar-brand" href="#">Morgan<span class="text-primary">Billingsley</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
