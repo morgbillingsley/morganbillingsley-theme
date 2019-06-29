c8999ae20dabcbe591a8560ece5b11c220d30f4c
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-28 21:14:05 -0700

    search bar i commit

--- header.php
+++ header.php
@@ -57,25 +57,40 @@
     <!-- End Facebook Pixel Code -->
 
     <div class="wrapper">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Morgan<span class="text-primary">Billingsley</span></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">My Work</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Blog</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary nav-shadow">
+
+            <a class="navbar-brand" href="#">MorganBillingsley</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="basicExampleNav">
+
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+
+                <?php get_search_form(); ?>
+
+            </div>
+
+        </nav>
---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form action="/" method="get" class="form-inline">
+    <div class="input-group">
+        <input class="search-bar bg-transparent" type="text" name="s" id="search" placeholder="Search" aria-label="Search in <?php echo home_url( '/' ); ?>" value="<?php the_search_query(); ?>">
+        <div class="input-group-append">
+            <button class="btn search-btn" type="submit"><i class="fas fa-search"></i></button>
+        </div>
+    </div>
+</form>
