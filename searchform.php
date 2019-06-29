121824fd0a4cde61b05686d5c7b6472e6017ec17
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-28 21:24:38 -0700

    search bar iii commit

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
     <div class="input-group">
         <input class="search-bar bg-transparent" type="text" name="s" id="search" placeholder="Search" aria-label="Search in <?php echo home_url( '/' ); ?>" value="<?php the_search_query(); ?>">
         <div class="input-group-append">
-            <button class="btn search-btn" type="submit"><i class="fas fa-search"></i></button>
+            <button class="btn search-btn" type="submit"><i class="icon ion-ios-search"></i></button>
         </div>
     </div>
 </form>
