fcbbd9252f94ada58b042af5e2c1f7353a0cd860
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-29 09:41:25 -0700

    single post ii commit

--- single.php
+++ single.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <?php if(have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
-            <?php if have_post_thumbnail() : ?>
-                <header class="masthead" style="background-image: url('https://via.placeholder.com/1900x1200'); padding: 100px 0px 75px 0px;">
+            <?php if(has_post_thumbnail()) : ?>
+                <header class="masthead" style="background-image: url('<?php get_the_post_thumbnail_url(); ?>'); padding: 100px 0px 75px 0px;">
             <?php else : ?>
                 <header class="masthead bg-secondary" style="padding: 100px 0px 75px 0px;">
             <?php endif; ?>
