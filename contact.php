fc7e66798e0fca550510aa4b5456b76ff5191d71
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-28 23:04:09 -0700

    new contact v commit

--- contact.php
+++ contact.php
@@ -23,39 +23,44 @@
         <div class="row">
 
             <div class="container">
-                <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
+                <form action="<?php echo esc_url(admin_url("admin-post.php")); ?>" method="POST">
+                    <input type="hidden" name="action" value="contact">
                     <div class="row">
 
-                        <div class="col-md-6 form-group">
-                        <label for="name">Full Name:</label>
-                        <input type="text" id="name" name="name" placeholder="Name" class="form-control">
+                        <div class="col-md-3 col-sm-6 form-group">
+                            <label for="name">First Name:</label>
+                            <input type="text" id="fname" name="fname" placeholder="First Name" class="form-control">
                         </div>
 
-                        <div class="col-md-6 form-group">
-                        <label for="name">Email Address:</label>
-                        <input type="email" id="email" name="email" placeholder="Email" class="form-control">
+                        <div class="col-md-3 col-sm-6 form-group">
+                            <label for="name">Last Name:</label>
+                            <input type="text" id="lname" name="lname" placeholder="Last Name" class="form-control">
                         </div>
-
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12 form-group">
-                        <label for="name">Phone Number:</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">+1</span>
+                        
+                        <div class="col-md-6 form-group">
+                            <label for="name">Phone Number:</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1">+1</span>
+                                </div>
+                                <input type="text" id="phone" name="phone" placeholder="Phone" class="form-control">
                             </div>
-                            <input type="text" id="phone" name="phone" placeholder="Phone" class="form-control">
                         </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <label for="name">Email Address:</label>
+                            <input type="email" id="email" name="email" placeholder="Email" class="form-control">
                         </div>
                     </div>
 
                     <div class="row">
 
                         <div class="col-md-12 form-group">
-                        <label for="name">Your Message:</label>
-                        <textarea type="text" id="message" name="message" placeholder="Message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="name">Your Message:</label>
+                            <textarea type="text" id="message" name="message" placeholder="Message" rows="2" class="form-control md-textarea"></textarea>
                         </div>
                     </div>
 
