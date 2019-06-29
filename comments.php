8bd233c1d9ecaa1c243e32f31d58bbbb45ef2d0e
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-29 11:46:47 -0700

    comment iv commit

--- comments.php
+++ comments.php
@@ -27,7 +27,10 @@
                 'title_reply'           => 'Write a Reply or Comment',
                 'title_reply_after'     => '</h4>',
                 'comment_notes_after'   => '',
-                'comment_field'         => '<p class="comment-form-comment mb-2 mt-2"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea class="form-control mb-2" id="comment" name="comment" rows="3" aria-required="true"></textarea></p>',
+                'comment_field'         => '<p class="comment-form-comment form-group"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea class="form-control" id="comment" name="comment" rows="3" aria-required="true"></textarea></p>',
+                'author'                => '<p class="comment-form-author form-group"><label for="author">' . _x( 'Name', 'noun' ) . '</label><input class="form-control" type="text" id="author" name="author" value size="30" maxlength="100" require="required"></p>',
+                'email'                => '<p class="comment-form-email form-group"><label for="email">' . _x( 'Email', 'noun' ) . '</label><input class="form-control" type="email" id="email" name="email" value size="30" maxlength="100" require="required"></p>',
+                'url'                => '<p class="comment-form-url form-group"><label for="url">' . _x( 'Website', 'noun' ) . '</label><input class="form-control" type="text" id="url" name="url" value size="30" maxlength="245"></p>'
         );
         comment_form($comments_args);
     ?>
