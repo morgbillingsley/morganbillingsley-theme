9f7f518855b7912aaa7b17a9575c9f53b85f06b7
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-29 22:10:34 -0700

    index xv commit

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
     <div class="container">
         <div class="text-center p-5">
-            <?php if(is_category()) : ?>
-                <h1><?php echo single_cat_title(); ?></h1>
-            <?php endif; ?> 
+            <h1><?php single_post_title(); ?></h1>
         </div>
         <?php get_template_part('content'); ?>
     </div>
