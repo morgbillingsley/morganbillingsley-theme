26f2f6bf03f93e4040bad0046c6d822f30d467a2
morgbillingsley <billingsley98.mb@gmail.com>
2019-06-29 23:16:20 -0700

    navbar v commit

--- header.php
+++ header.php
@@ -96,7 +96,7 @@
 
         </nav>
         <?php $menu = get_menu('primary'); ?>
-        <?php echo $menu['primary']; ?>
+        <?php echo $menu['primary']['ID']; ?>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary nav-shadow">
 
             <a class="navbar-brand" href="#">MorganBillingsley</a>
